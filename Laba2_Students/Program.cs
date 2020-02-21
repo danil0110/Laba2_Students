7c6b3f26bc0912976dfd8eff2017e58209de8353
Danil <danilmail0110@gmail.com>
2020-02-21 11:23:24 +0200

    Add directory reading

---
+++ Laba2_Students/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Laba2_Students
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string directory = Directory.GetCurrentDirectory() + $"\\{Console.ReadLine()}\\";
+            string[] filePaths = Directory.GetFiles(directory, "*.csv");
+        }
+    }
+}
