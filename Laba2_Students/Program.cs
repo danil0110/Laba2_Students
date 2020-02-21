2a57f792b2ee5af8eedb5a99428788dce6961e32
Danil <danilmail0110@gmail.com>
2020-02-21 20:33:17 +0200

    Fixed array of marks and add a average count

--- Laba2_Students/Program.cs
+++ Laba2_Students/Program.cs
@@ -11,30 +11,31 @@ namespace Laba2_Students
             string directory = Directory.GetCurrentDirectory() + $"\\{Console.ReadLine()}\\";
             string[] filePaths = Directory.GetFiles(directory, "*.csv");
             List<Students> students = new List<Students>();
-            int count = 0;
-            
+
             foreach (string path in filePaths)
             {
                 StreamReader sr = new StreamReader(path);
-                int n = Convert.ToInt32(sr.ReadLine());
+                int n = Convert.ToInt32(sr.ReadLine()),
+                    count = 0;
                 string line;
+
                 while (count != n)
                 {
                     line = sr.ReadLine();
                     students.Add(new Students() { name = line });
-                    string[] temp = students[count].name.Split(',');
+                    string[] temp = students[students.Count - 1].name.Split(',');
                     if (temp[6] == "TRUE")
                     {
-                        students.RemoveAt(count);
+                        students.RemoveAt(students.Count - 1);
                         n--;
                         continue;
                     }
-                    students[count].name = temp[0];
+                    students[students.Count - 1].name = temp[0];
                     for (int i = 1; i < 6; i++)
-                    {
-                        students[count].grade[i - 1] = Convert.ToInt32(temp[i]);
-                        Console.Write(students[count].grade[i - 1] + " ");
-                    }
+                        students[students.Count - 1].grade[i - 1] = Convert.ToInt32(temp[i]);
+
+                    students[students.Count - 1].average = students[students.Count - 1].AverageCount(students[students.Count - 1].grade);
+                    
                     count++;
                 }
             }
--- Laba2_Students/Students.cs
+++ Laba2_Students/Students.cs
@@ -1,9 +1,19 @@
-﻿namespace Laba2_Students
+﻿using System;
+
+namespace Laba2_Students
 {
     public class Students
     {
         public string name;
         public int[] grade = new int[5];
-        public float average;
+        public double average;
+
+        public double AverageCount(int[] grade)
+        {
+            double sum = 0;
+            foreach (var mark in grade)
+                sum += mark;
+            return Math.Round(Convert.ToDouble(sum / 5), 3);
+        }
     }
 }
